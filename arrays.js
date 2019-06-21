d43e82cecea7c640869d66a6618c77d7020c33cc
Aaron M <amitchtech@gmail.com>
2019-06-21 20:39:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-   var newArray = array.unshift(element);
+   var newArray = element.unshift(array);
   return newArray
 }
